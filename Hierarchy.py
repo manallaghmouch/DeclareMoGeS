3b2189eee510842d4112d3df6e74f4313e5a82e8
manallaghmouch <68498975+manallaghmouch@users.noreply.github.com>
2023-06-08 17:55:51 +0200

    fix

--- Hierarchy.py
+++ Hierarchy.py
@@ -5,7 +5,6 @@ import random
 class Hierarchy: 
     """This class defines hierarchies between Declare constraints in terms of specialisation"""
 
-    # obv stronger-weaker relations Schunselaars: enkel met identiek dezelfde antecedent en consequent!
     specialisation_candidates = {
         'Precedence': [Init, AlternatePrecedence, ChainPrecedence, Succession, AlternateSuccession, ChainSuccession],
         'Response': [AlternateResponse, ChainResponse, Succession, AlternateSuccession, ChainSuccession],
@@ -53,10 +52,10 @@ class Hierarchy:
             reaction = constraint.get_reaction()
             specialized_template = random.choice(Hierarchy.specialisation_candidates[constraint.__class__.__name__])
             #specialized_constraint = specialized_template(action, reaction)
-            if specialized_template == CoExistence | specialized_template == Succession | specialized_template == AlternateSuccession | \
-                specialized_template == ChainSuccession:
+            if specialized_template == "CoExistence" or specialized_template == "Succession" or specialized_template == "AlternateSuccession" or \
+                specialized_template == "ChainSuccession":
                 specialized_constraint = random.choice[specialized_template(action, reaction), specialized_template(reaction, action)]
-            elif specialized_template == Response | specialized_template == AlternateResponse | specialized_template == ChainResponse:
+            elif specialized_template == "Response" or specialized_template == "AlternateResponse" or specialized_template == "ChainResponse":
                 specialized_constraint = specialized_template(action, reaction) 
             else: 
                 specialized_constraint = specialized_template(reaction, action)
@@ -67,28 +66,7 @@ class Hierarchy:
             specialized_template = random.choice(Hierarchy.specialisation_candidates[constraint.__class__.__name__])
             specialized_constraint = specialized_template(action)
 
-        # case 'Absence':
-        #     action = constraint.get_action()
-        #     # n = constraint.get_n()
-        #     specialized_template = random.choice(Hierarchy.specialisation_candidates[constraint.__class__.__name__])
-        #     if specialized_template == Absence:
-        #         specialized_constraint = random.choice([specialized_template(action,n),specialized_template(action,n - random.randint(1,n-1))])
-        #     elif specialized_template == Exactly: 
-        #         if n>1: 
-        #             specialized_constraint = specialized_template(action, n-1) # instellen dat het nooit minder dan 1 mag zijn!
-        #         else:
-        #             pass # nog aanpassen -- nu slaat die dit geval over -- idealiter wil ik dat er dan opnieuw een specialisatie wordt gegenereerd
-
-        # case 'Existence':
-        #     action = constraint.get_action()
-        #     # n = constraint.get_n()
-        #     specialized_template = random.choice(Hierarchy.specialisation_candidates[constraint.__class__.__name__])
-        #     if specialized_template == Exactly:
-        #         specialized_constraint = specialized_template(action,n)
-        #     elif specialized_template == Existence: 
-        #         specialized_constraint = specialized_template(action,n + random.randint(1,4))
-
-        else: # Voor alle andere cases nemen we dezelfde constraint over in het specialized_model (vb. init)
+        else: 
             specialized_constraint = False                  
 
         return specialized_constraint
@@ -97,11 +75,3 @@ class Hierarchy:
         if self.generate_specialisation_candidate(constraint) == False:
             return False
         else: return True 
-
-
-# Test
-# hierarchy = Hierarchy()
-# #model = [Response('a','b'), Precedence('c','d'), AlternatePrecedence('e','b'), RespondedExistence('k','i')]
-# model1 = [RespondedExistence('k','i'), Precedence('a','b'), Init('b'), Init('a')] # We gaan er hier van uit dat de user een correct model meegeeft, want juiste 
-# # activiteiten worden niet meer gecontroleerd
-# hierarchy.specialise_model(model1)
