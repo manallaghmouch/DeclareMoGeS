e5f728a4bf5e7d75b30b2e289cdf47ea98ade0e6
manallaghmouch <manal.laghmouch@uhasselt.be>
2023-11-17 14:13:26 +0100

    fix

--- test_setting2.py
+++ test_setting2.py
@@ -54,7 +54,7 @@ weights = [random.randint(1,10),random.randint(1,10),random.randint(1,10),random
 
 # Main program
 st_g = time.time()
-model = Model("model6.decl", alphabet_size=int(argv[2]), set_size=int(argv[1]), weights=weights, consequent_not_adding=stop_after, time_out=int(argv[3]), templates=templates)
+model = Model("model6.decl", alphabet_size=alphabet, set_size=set, weights=weights, consequent_not_adding=stop_after, time_out=stop_time, templates=templates)
 et_g = time.time()
 
 # specialization scenarios
